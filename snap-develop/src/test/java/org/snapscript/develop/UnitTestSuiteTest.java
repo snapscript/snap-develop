0cf718cb2cfa4b231f06fcff5590e78fbc09d670
Niall Gallagher <gallagher_niall@yahoo.com>
2016-08-21 18:27:19 +0100

    Add clover

---
+++ snap-develop/src/test/java/org/snapscript/develop/UnitTestSuiteTest.java
@@ -0,0 +1,32 @@
+package org.snapscript.develop;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+import org.snapscript.compile.Compiler;
+import org.snapscript.compile.Executable;
+import org.snapscript.compile.ResourceCompiler;
+import org.snapscript.compile.StoreContext;
+import org.snapscript.core.Context;
+import org.snapscript.core.store.FileStore;
+import org.snapscript.core.store.Store;
+
+public class UnitTestSuiteTest extends TestCase {
+   
+   public void testSuite() throws Exception {
+      String variable = System.getenv("PROJECT_ROOT");
+      
+      if(variable == null) {
+         throw new IllegalStateException("Environment variable PROJECT_ROOT not set");
+      }
+      File root = new File(variable, "test");
+      Store store = new FileStore(root);
+      Context context = new StoreContext(store);
+      Compiler compiler = new ResourceCompiler(context);
+      Executable executable = compiler.compile("run.snap");
+
+      executable.execute();
+   }
+
+}
