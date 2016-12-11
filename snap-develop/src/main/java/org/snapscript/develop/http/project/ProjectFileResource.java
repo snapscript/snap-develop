2ffbccc30658d2c5e10ef8599258ca3603074ecf
Niall Gallagher <gallagher_niall@yahoo.com>
2016-12-11 15:19:10 +0000

    Show comment for missing scripts

--- snap-develop/src/main/java/org/snapscript/develop/http/project/ProjectFileResource.java
+++ snap-develop/src/main/java/org/snapscript/develop/http/project/ProjectFileResource.java
@@ -1,10 +1,13 @@
 package org.snapscript.develop.http.project;
 
 import java.io.OutputStream;
+import java.io.PrintStream;
 
 import org.simpleframework.http.Path;
 import org.simpleframework.http.Request;
 import org.simpleframework.http.Response;
+import org.simpleframework.http.Status;
+import org.snapscript.core.Reserved;
 import org.snapscript.develop.http.resource.ContentTypeResolver;
 import org.snapscript.develop.http.resource.Resource;
 
@@ -24,12 +27,26 @@ public class ProjectFileResource implements Resource {
       String projectPath = path.getPath(2); // /<project-name>/<project-path> or /default/blah.snap
       Project project = builder.createProject(path);
       ProjectFileSystem fileSystem = project.getFileSystem();
-      byte[] resource = fileSystem.readAsByteArray(projectPath);
       String type = resolver.resolveType(projectPath);
-      OutputStream out = response.getOutputStream();
       
+      response.setStatus(Status.OK);
       response.setContentType(type);
-      out.write(resource);
-      out.close();
+      
+      try {
+         byte[] resource = fileSystem.readAsByteArray(projectPath);
+         OutputStream out = response.getOutputStream();
+         
+         out.write(resource);
+         out.close();
+      }catch(Exception e) {
+         PrintStream out = response.getPrintStream();
+         response.setStatus(Status.NOT_FOUND);
+         
+         if(projectPath.endsWith(Reserved.SCRIPT_EXTENSION)){
+            response.setStatus(Status.OK); // just to display .snap comment
+            out.println("// No source found for " + projectPath);
+         }
+         out.close();
+      }
    }
 }
