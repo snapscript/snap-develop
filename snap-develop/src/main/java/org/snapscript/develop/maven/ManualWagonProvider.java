a79737e2e86269b962b7459a6c4efe23fd496097
Niall Gallagher <gallagher_niall@yahoo.com>
2017-01-29 16:55:10 +0000

    Fix maven client

--- snap-develop/src/main/java/org/snapscript/develop/maven/ManualWagonProvider.java
+++ snap-develop/src/main/java/org/snapscript/develop/maven/ManualWagonProvider.java
@@ -2,6 +2,7 @@ package org.snapscript.develop.maven;
 
 import org.apache.maven.wagon.Wagon;
 import org.apache.maven.wagon.providers.http.LightweightHttpWagon;
+import org.apache.maven.wagon.providers.http.LightweightHttpWagonAuthenticator;
 import org.sonatype.aether.connector.wagon.WagonProvider;
 
 public class ManualWagonProvider implements WagonProvider {
@@ -11,7 +12,11 @@ public class ManualWagonProvider implements WagonProvider {
    @Override
    public Wagon lookup(String roleHint) throws Exception {
       if (SCHEME.equals(roleHint)) {
-         return new LightweightHttpWagon();
+         LightweightHttpWagonAuthenticator authenticator = new LightweightHttpWagonAuthenticator();
+         LightweightHttpWagon wagon = new LightweightHttpWagon();
+         
+         wagon.setAuthenticator(authenticator);
+         return wagon;
       }
       return null;
    }
