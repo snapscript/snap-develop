15ddc483bd52280ee0d91ec116d87bbbeac98708
Niall Gallagher <gallagher_niall@yahoo.com>
2017-02-02 19:22:32 +0000

    Add socket tunnel for HTTP CONNECT

---
+++ snap-agent/src/main/java/org/snapscript/agent/event/socket/SocketTunnel.java
@@ -0,0 +1,68 @@
+package org.snapscript.agent.event.socket;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+public class SocketTunnel {
+
+   private static final int[] TERMINAL = {'\r', '\n', '\r', '\n'};
+   private static final String CHARSET = "UTF-8";
+   private static final String FORMAT = "EEE, dd MMM yyyy HH:mm:ss z";
+   private static final String TIME_ZONE = "GMT";
+   private static final String REQUEST = 
+   "CONNECT %s:%s HTTP/1.1\r\n" +
+   "Host: %s:%s\r\n" +
+   "Date: %s\r\n"+
+   "\r\n";
+
+   private final DateFormat format;
+   private final TimeZone zone;
+   private final int connect;
+   
+   public SocketTunnel(int connect) {
+      this.format = new SimpleDateFormat(FORMAT);
+      this.zone = TimeZone.getTimeZone(TIME_ZONE);
+      this.connect = connect;
+   }
+   
+   public synchronized void tunnel(Socket socket) throws Exception{
+      format.setTimeZone(zone);
+      send(socket);
+      receive(socket);
+   }
+   
+   private synchronized void send(Socket socket) throws Exception {
+      OutputStream output = socket.getOutputStream();
+      InetAddress address = socket.getInetAddress();
+      String host = address.getHostAddress();
+      int port = socket.getPort();
+      long time = System.currentTimeMillis();
+      String date = format.format(time);
+      String request = String.format(REQUEST, host, connect, host, port, date);
+      byte[] header = request.getBytes(CHARSET);
+      
+      output.write(header);
+      output.flush();
+   }
+   
+   private synchronized void receive(Socket socket) throws Exception {
+      InputStream input = socket.getInputStream();
+      int seek = 0;
+
+      while(true) {
+         int count = input.read();
+         
+         if(count != TERMINAL[seek++]) {
+            seek = 0;
+         }
+         if(seek == TERMINAL.length) {
+            break;
+         }
+      }
+   }
+}
