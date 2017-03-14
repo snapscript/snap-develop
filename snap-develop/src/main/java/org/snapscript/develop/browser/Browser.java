be80d273877e69cdde9690494d80464c8625954e
Niall Gallagher <gallagher_niall@yahoo.com>
2017-03-14 19:46:44 +0000

    Enable client debugger

--- snap-develop/src/main/java/org/snapscript/develop/browser/Browser.java
+++ snap-develop/src/main/java/org/snapscript/develop/browser/Browser.java
@@ -10,20 +10,18 @@ import javafx.scene.layout.Region;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
-import org.snapscript.agent.log.ProcessLogger;
-
 public class Browser extends Region {
 
-   private final ProcessLogger logger;
+   private final BrowserContext context;
    private final WebEngine engine;
    private final WebView browser;
-   
-   public Browser(ProcessLogger logger) {
+
+   public Browser(BrowserContext context) {
       this.browser = new WebView();
       this.engine = browser.getEngine();
-      this.logger = logger;
+      this.context = context;
    }
-   
+
    public void show(String target) {
       engine.getLoadWorker().stateProperty().addListener(new ChangeListener() {
 
@@ -33,25 +31,37 @@ public class Browser extends Region {
                Object window = engine.executeScript("window");
                Class type = window.getClass();
                Method method = type.getDeclaredMethod("setMember", String.class, Object.class);
-               
+
                method.setAccessible(true);
                method.invoke(window, "java", Browser.this);
                engine.executeScript("console.log = function(message){java.log(message);};\n");
-            }catch(Exception e) {
-               logger.info("Could not register log listener", e);
+            } catch (Exception e) {
+               context.getLogger().info("Could not register log listener", e);
             }
          }
-         
+
+      });
+      engine.documentProperty().addListener(new ChangeListener() {
+         @Override
+         public void changed(ObservableValue prop, Object oldDoc, Object newDoc) {
+            enableFirebug(engine);
+         }
       });
       getStyleClass().add("browser");
       engine.load(target);
       getChildren().add(browser);
    }
 
-   public void log(Object message) {
-      logger.info(String.valueOf(message));
+   private void enableFirebug(WebEngine engine) {
+      if(context.isDebug()) {
+         engine.executeScript("if (!document.getElementById('FirebugLite')){E = document['createElement' + 'NS'] && document.documentElement.namespaceURI;E = E ? document['createElement' + 'NS'](E, 'script') : document['createElement']('script');E['setAttribute']('id', 'FirebugLite');E['setAttribute']('src', 'https://getfirebug.com/' + 'firebug-lite.js' + '#startOpened');E['setAttribute']('FirebugLite', '4');(document['getElementsByTagName']('head')[0] || document['getElementsByTagName']('body')[0]).appendChild(E);E = new Image;E['setAttribute']('src', 'https://getfirebug.com/' + '#startOpened');}");
+      }
    }
-   
+
+   public void log(Object message) {
+      context.getLogger().info(String.valueOf(message));
+   }
+
    @Override
    protected void layoutChildren() {
       double width = getWidth();
