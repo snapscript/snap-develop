0cb6fb4607fe0d519ba416215590582b5f3a3de9
Niall Gallagher <gallagher_niall@yahoo.com>
2017-05-28 13:46:55 +0100

    Add drag and drop

--- snap-develop/src/main/resources/resource/js/tree.js
+++ snap-develop/src/main/resources/resource/js/tree.js
@@ -15,7 +15,7 @@ var FileTree;
                 url: requestPath,
                 success: function (response) {
                     $('#' + element).html(response);
-                    showFancyTree(id, treeMenuHandler, clickCallback); // show the fancy tree
+                    showFancyTree(id, !foldersOnly, treeMenuHandler, clickCallback); // show the fancy tree
                 },
                 async: true
             });
@@ -30,16 +30,58 @@ var FileTree;
         }
     }
     FileTree.showTreeNode = showTreeNode;
-    function showFancyTree(id, treeMenuHandler, clickCallback) {
+    function showFancyTree(id, dragAndDrop, treeMenuHandler, clickCallback) {
+        var dnd = {
+            autoExpandMS: 400,
+            focusOnClick: true,
+            preventVoidMoves: true,
+            preventRecursiveMoves: true,
+            dragStart: function (node, data) {
+                /** This function MUST be defined to enable dragging for the tree.
+                 *  Return false to cancel dragging of node.
+                 */
+                return true;
+            },
+            dragEnter: function (node, data) {
+                /** data.otherNode may be null for non-fancytree droppables.
+                 *  Return false to disallow dropping on node. In this case
+                 *  dragOver and dragLeave are not called.
+                 *  Return 'over', 'before, or 'after' to force a hitMode.
+                 *  Return ['before', 'after'] to restrict available hitModes.
+                 *  Any other return value will calc the hitMode from the cursor position.
+                 */
+                // Prevent dropping a parent below another parent (only sort
+                // nodes under the same parent)
+                /* if(node.parent !== data.otherNode.parent){
+                  return false;
+                }
+                // Don't allow dropping *over* a node (would create a child)
+                return ["before", "after"];
+                */
+                return true;
+            },
+            dragDrop: function (node, data) {
+                /** This function MUST be defined to enable dropping of items on
+                 *  the tree.
+                 */
+                data.otherNode.moveTo(node, data.hitMode);
+            }
+        };
         // using default options
         $('#' + id).fancytree({
+            extensions: dragAndDrop ? ["dnd"] : [],
             click: clickCallback,
             expand: function (event, data) {
-                Command.folderExpand(data.node.key);
+                if (typeof Command !== 'undefined') {
+                    Command.folderExpand(data.node.key);
+                }
             },
             collapse: function (event, data) {
-                Command.folderCollapse(data.node.key);
-            }
+                if (typeof Command !== 'undefined') {
+                    Command.folderCollapse(data.node.key);
+                }
+            },
+            dnd: dragAndDrop ? dnd : null
         });
         if (treeMenuHandler != null) {
             $("#" + id).contextmenu({
