e7f33b62c3ede9a023a7e3f17bb32d5c3b3ecab2
Niall Gallagher <gallagher_niall@yahoo.com>
2017-11-27 21:20:49 +0000

    Archive old images

--- snap-studio/src/main/java/org/snapscript/studio/browser/ChromiumApplication.java
+++ snap-studio/src/main/java/org/snapscript/studio/browser/ChromiumApplication.java
@@ -1,12 +1,17 @@
 package org.snapscript.studio.browser;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import lombok.extern.slf4j.Slf4j;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.snapscript.studio.common.ClassPathReader;
 import org.snapscript.studio.ui.swt.Chromium;
 
 @Slf4j
@@ -17,13 +22,23 @@ public class ChromiumApplication {
       Shell shell = new Shell(display);
       FillLayout layout = new FillLayout();
 
+      try {
+         if (isPlatformWindows()) {
+            byte[] data = ClassPathReader.findResource(context.getIconPath());
+            InputStream stream = new ByteArrayInputStream(data);
+            Image small = new Image(display, stream);
+            shell.setImage(small);
+         }
+      } catch (Exception e) {
+         log.info("Could not set image", e);
+      }
       shell.setLayout(layout);
 
       Chromium browser = new Chromium(shell, SWT.NONE);
-      
+
       try {
          shell.setText(context.getDirectory().getCanonicalPath());
-      }catch(Exception e){
+      } catch (Exception e) {
          log.info("Could not set title", e);
       }
       browser.setUrl(context.getTarget());
@@ -37,5 +52,10 @@ public class ChromiumApplication {
          }
       }
       display.dispose();
+   }
+
+   private static boolean isPlatformWindows() {
+      String platform = System.getProperty("os.name");
+      return platform != null && platform.contains("indows");
    }
 }
