3e47d7447a3fbccd0fa346c4589dd084c8a6fc77
Niall Gallagher <gallagher_niall@yahoo.com>
2017-12-03 18:33:36 +0000

    Update parameter index

--- snap-studio-index/src/main/java/org/snapscript/studio/index/tree/ParameterDeclarationIndex.java
+++ snap-studio-index/src/main/java/org/snapscript/studio/index/tree/ParameterDeclarationIndex.java
@@ -11,6 +11,7 @@ import org.snapscript.core.Value;
 import org.snapscript.core.function.Parameter;
 import org.snapscript.studio.index.IndexResult;
 import org.snapscript.tree.Modifier;
+import org.snapscript.tree.ModifierList;
 import org.snapscript.tree.NameReference;
 import org.snapscript.tree.annotation.AnnotationList;
 import org.snapscript.tree.constraint.Constraint;
@@ -22,20 +23,20 @@ public class ParameterDeclarationIndex implements Compilation  {
    private final Evaluation identifier;
    private final Constraint constraint;
    
-   public ParameterDeclarationIndex(AnnotationList annotations, Evaluation identifier){
-      this(annotations, identifier, null, null);
+   public ParameterDeclarationIndex(AnnotationList annotations, ModifierList modifiers, Evaluation identifier){
+      this(annotations, modifiers, identifier, null, null);
    }
    
-   public ParameterDeclarationIndex(AnnotationList annotations, Evaluation identifier, Constraint constraint){
-      this(annotations, identifier, null, constraint);
+   public ParameterDeclarationIndex(AnnotationList annotations, ModifierList modifiers, Evaluation identifier, Constraint constraint){
+      this(annotations, modifiers, identifier, null, constraint);
    }
    
-   public ParameterDeclarationIndex(AnnotationList annotations, Evaluation identifier, Modifier modifier){
-      this(annotations, identifier, modifier, null);
+   public ParameterDeclarationIndex(AnnotationList annotations, ModifierList modifiers, Evaluation identifier, Modifier modifier){
+      this(annotations, modifiers, identifier, modifier, null);
    }
    
-   public ParameterDeclarationIndex(AnnotationList annotations, Evaluation identifier, Modifier modifier, Constraint constraint){
-      this.declaration = new IndexParameterDeclaration(annotations, identifier, modifier, constraint);
+   public ParameterDeclarationIndex(AnnotationList annotations, ModifierList modifiers, Evaluation identifier, Modifier modifier, Constraint constraint){
+      this.declaration = new IndexParameterDeclaration(annotations, modifiers, identifier, modifier, constraint);
       this.identifier = identifier;
       this.constraint = constraint;
    }
@@ -62,8 +63,8 @@ public class ParameterDeclarationIndex implements Compilation  {
       private final NameReference reference;
       private final Modifier modifier;
       
-      public IndexParameterDeclaration(AnnotationList annotations, Evaluation identifier, Modifier modifier, Constraint constraint){
-         super(annotations, identifier, modifier, constraint);
+      public IndexParameterDeclaration(AnnotationList annotations, ModifierList modifiers, Evaluation identifier, Modifier modifier, Constraint constraint){
+         super(annotations, modifiers, identifier, modifier, constraint);
          this.reference = new NameReference(identifier);
          this.modifier = modifier;
       }
